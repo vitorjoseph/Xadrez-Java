2262414d8912b889cf6ae1335d024924252d23e0
Vítor José <vitorjoselara@gmail.com>
2023-03-29 13:08:39 -0300

    Position

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
 
